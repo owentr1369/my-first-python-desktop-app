2c8eb727576ba0f6c019adaea7bc6130234bff2a
TranTam <tamvaa1306@gmail.com>
2023-12-09 20:46:04 +0700

    add button to app

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from tkinter import *
 
 window = Tk()
 
-window.title("Hello I'm Owen")
+window.title("Hello Uyen Beo")
 
-lbl = Label(window, text="Hello I'm Owen", font=("Arial Bold", 50))
+window.geometry('350x200')
+
+lbl = Label(window, text="Hello Uyen Beo hihi", font=("Arial Bold", 20))
 
 lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Click Me", bg="orange", fg="red")
+btn.grid(column=1, row=0)
 
 window.mainloop()
