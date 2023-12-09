e9161cc3778cbfa75bcd46bf8dee0db0a868be05
TranTam <tamvaa1306@gmail.com>
2023-12-09 20:51:29 +0700

    text input and get its value

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ lbl = Label(window, text="Hello Uyen Beo hihi", font=("Arial Bold", 20))
 
 lbl.grid(column=0, row=0)
 
+txt = Entry(window, width=10)
+
+txt.grid(column=1, row=0)
+
 def clicked():
-    lbl.configure(text="Button was clicked !!")
+    res = "Welcome to " + txt.get()
+    lbl.configure(text=res)
 
 btn = Button(window, text="Click Me", bg="orange", fg="red", command=clicked)
-btn.grid(column=1, row=0)
+btn.grid(column=2, row=0)
 
 window.mainloop()
