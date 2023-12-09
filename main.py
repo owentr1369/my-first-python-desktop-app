8cc43f75a619e708ec549e1876d0d26c8890c993
TranTam <tamvaa1306@gmail.com>
2023-12-09 20:47:17 +0700

    on lick button

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ lbl = Label(window, text="Hello Uyen Beo hihi", font=("Arial Bold", 20))
 
 lbl.grid(column=0, row=0)
 
-btn = Button(window, text="Click Me", bg="orange", fg="red")
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+
+btn = Button(window, text="Click Me", bg="orange", fg="red", command=clicked)
 btn.grid(column=1, row=0)
 
 window.mainloop()
